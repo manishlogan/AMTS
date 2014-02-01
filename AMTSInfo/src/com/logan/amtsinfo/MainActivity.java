c6a88d98b3f16392c025688449d40a4def3fa907
Manish Logan <manishjain.forever@gmail.com>
2014-02-01 18:37:45 +0530

    Changed start app APP ID

--- AMTSInfo/src/com/logan/amtsinfo/MainActivity.java
+++ AMTSInfo/src/com/logan/amtsinfo/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		StartAppAd.init(this, "111147141", "211077704");
+		StartAppAd.init(this, "111147141", "201107033");
 		setContentView(R.layout.activity_main);
 		activity = this;
 		
